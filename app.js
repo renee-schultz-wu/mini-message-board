7ebe45e3b9c064dee163005c59dbb34203f4e047
renee <50965960+wurenee@users.noreply.github.com>
2024-09-21 23:18:18 -0700

    Add feature to create messages

--- app.js
+++ app.js
@@ -3,14 +3,39 @@ const path = require("node:path");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-const indexRouter = require("./routes/indexRouter")
-const newRouter = require("./routes/newRouter")
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+  ];
+  
+app.get("/", (req, res) => {
+    res.render("index", { title: "Mini Messageboard" , messages: messages });
+});
 
 
-app.use("/", indexRouter);
+app.get("/new", (req, res) => {
+    res.render("form", {})
+})
+
+app.post("/new", (req, res) => {
+    messages.push({ text: req.body.message, user: req.body.username, added: new Date() });
+    res.redirect("/")
+})
+
+
 
 
 const PORT = 3000;
